9eedd19a1cb2ed0465f99c2044d13b88b0001ef9
Supriyareddy1906 <167641305+Supriyareddy1906@users.noreply.github.com>
2024-04-22 12:26:46 +0530

    Create bitwise operators.py

---
+++ bitwise operators.py
@@ -0,0 +1,39 @@
+a=~5
+print(a)
+print(~a)
+
+a=-999
+print(~a)
+
+a+=10
+print(a)
+
+a-=10
+print(a)
+
+a*=10
+print(a)
+
+a/=10
+print(a)
+
+a//=10
+print(a)
+
+a=25
+a=a<<5
+print(a)
+
+a=20
+a=a>>5
+print(a)
+print(type(a))
+
+print(a>10 and a>2)
+print(a>-10 or a>-1)
+
+n='sup'
+print('p' in n)
+
+a=10
+print(5 in 10)
