6e300d73ced9cbbedf743e76173291afa83d9899
Supriyareddy1906 <167641305+Supriyareddy1906@users.noreply.github.com>
2024-04-22 12:33:49 +0530

    Create def.py

---
+++ def.py
@@ -0,0 +1,13 @@
+def sup():
+    print("hello sup")
+def sam():
+    print("hello sam")
+def san():
+    print("hello san")
+def chinnu():
+    print("hello chinnu")
+sup()
+sam()
+san()
+chinnu()
+print()
