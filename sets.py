6636412d8b55391e43b8ce95b8a1acc313d26428
Supriyareddy1906 <167641305+Supriyareddy1906@users.noreply.github.com>
2024-04-24 09:32:41 +0530

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,8 @@
+states = {"goa","mp","up","tamilnadu","karnataka","mumbai","kashmir"}
+print(states)
+print(type(states))
+print("looping through the set elements...")
+for i in states:
+    print(i)
+
+    
